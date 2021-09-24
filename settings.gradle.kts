d8b004fadefe445bc67da39cc0c033e93fa1a58d
Erik <Erik@ErikSchouten.com>
2021-09-24 18:10:08 +0200

    Upgraded versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,18 +19,18 @@ enableFeaturePreview("VERSION_CATALOGS")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            version("coroutines", "1.5.1")
-            version("ktor", "1.6.2")
-            version("logback", "1.2.5")
+            version("coroutines", "1.5.2")
+            version("ktor", "1.6.3")
+            version("logback", "1.2.6")
             version("koin", "3.0.2")
             version("kgraphql", "0.17.14")
-            version("exposed", "0.32.1")
+            version("exposed", "0.35.1")
             version("jwt", "3.18.1")
             version("bcrypt", "0.9.0")
             version("hikari", "5.0.0")
-            version("mariadb", "2.7.3")
+            version("mariadb", "2.7.4")
             version("h2", "1.4.200")
-            version("flyway", "7.12.1")
+            version("flyway", "7.15.0")
             version("mockk", "1.12.0")
         }
     }
