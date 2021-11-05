0aeabbaed7e6fc1369c7c6a1f4f7be739d73a06a
Erik <Erik@ErikSchouten.com>
2021-11-05 20:26:07 +0100

    Updated versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,10 +19,10 @@ enableFeaturePreview("VERSION_CATALOGS")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            version("kotlin", "1.6.0-RC")
+            version("kotlin", "1.6.0-RC2")
             version("shadowjar", "7.1.0")
             version("coroutines", "1.5.2")
-            version("ktor", "1.6.4")
+            version("ktor", "1.6.5")
             version("logback", "1.3.0-alpha10")
             version("koin", "3.1.3")
             version("kgraphql", "0.17.14")
@@ -32,7 +32,7 @@ dependencyResolutionManagement {
             version("hikari", "5.0.0")
             version("mariadb", "2.7.4")
             version("h2", "1.4.200")
-            version("flyway", "8.0.2")
+            version("flyway", "8.0.3")
             version("mockk", "1.12.0")
             version("updateversions", "0.39.0")
         }
