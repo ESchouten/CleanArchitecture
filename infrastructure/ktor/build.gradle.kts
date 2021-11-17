6336e94d8d9b023d7cf450beba3f239c158205c9
Erik <Erik@ErikSchouten.com>
2021-11-17 20:07:21 +0100

    Cookie auth base

--- infrastructure/ktor/build.gradle.kts
+++ infrastructure/ktor/build.gradle.kts
@@ -33,8 +33,10 @@ dependencies {
 
     implementation("io.ktor", "ktor-server-core", libs.versions.ktor.get())
     implementation("io.ktor", "ktor-server-cio", libs.versions.ktor.get())
+    implementation("io.ktor", "ktor-gson", libs.versions.ktor.get())
     implementation("io.ktor", "ktor-auth", libs.versions.ktor.get())
     implementation("io.ktor", "ktor-auth-jwt", libs.versions.ktor.get())
+    implementation("io.ktor", "ktor-server-sessions", libs.versions.ktor.get())
     implementation("ch.qos.logback", "logback-classic", libs.versions.logback.get())
     implementation("io.insert-koin", "koin-ktor", libs.versions.koin.get())
     implementation("com.apurebase", "kgraphql-ktor", libs.versions.kgraphql.get())
