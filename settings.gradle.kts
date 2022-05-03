d05904f516eba3828e2a4851a888c869c2f26416
Erik <Erik@ErikSchouten.com>
2022-05-03 16:17:17 +0200

    Dependency updates

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ dependencyResolutionManagement {
             version("kotlin", "1.6.21")
             version("shadowjar", "7.1.2")
             version("coroutines", "1.6.1")
-            version("ktor", "2.0.0")
+            version("ktor", "2.0.1")
             version("logback", "1.3.0-alpha14")
             version("koin", "3.2.0-beta-1")
             version("kgraphql", "0.17.14")
@@ -31,7 +31,7 @@ dependencyResolutionManagement {
             version("hikari", "5.0.1")
             version("mariadb", "3.0.4")
             version("h2", "2.1.212")
-            version("flyway", "8.5.8")
+            version("flyway", "8.5.10")
             version("mockk", "1.12.3")
             version("updateversions", "0.42.0")
         }
