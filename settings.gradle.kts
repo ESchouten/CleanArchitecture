066e985ddc66ce2852d4a0874666b1ab47247636
Erik Schouten <erik@erikschouten.com>
2023-04-18 16:39:04 +0200

    Updated gradle + kompendium

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,7 +25,7 @@ dependencyResolutionManagement {
             version("shadowjar", "8.1.1")
             version("coroutines", "1.7.0-Beta")
             version("ktor", "2.2.4")
-            version("kompendium", "3.13.0")
+            version("kompendium", "3.14.0")
             version("slf4j", "2.0.7")
             version("logback", "1.4.6")
             version("koin", "3.4.0")
@@ -37,7 +37,7 @@ dependencyResolutionManagement {
             version("mariadb", "3.1.3")
             version("h2", "2.1.214")
             version("flyway", "9.16.3")
-            version("mockk", "1.13.4")
+            version("mockk", "1.13.5")
             version("reflections", "0.10.2")
             version("updateversions", "0.46.0")
         }
